4389622114c02c4c8ee5569dbf111996f4414a89
Shelby Jacobs <shelbyvjacobs@gmail.com>
2019-11-01 10:16:55 -0400

    added stories to thumbnails block group

--- src/components/Thumbnails Block/Thumbnails.block.js
+++ src/components/Thumbnails Block/Thumbnails.block.js
@@ -4,12 +4,38 @@ import { conditionalExpression } from '@babel/types';
 
 const Block= (props) => {
     let classList = ''
-    let types = ['columnWhite']
+    let types = ['column', 'doubleColumn', 'smallThumbnail']
     // conditional styling
     if (types.includes(props.type)) {
         classList += ` block-${props.type}`
     }
-    return <div><h1>Hey there</h1></div>
+    if (props.white) {
+        classList += ` white`
+    }
+    if (props.grey) {
+        classList += ` grey`
+    }
+    if (props.type == "column"){
+        return(
+            <div>
+                {/* insert jsx for the 1 column thumbnail here */}
+            </div>
+        )
+    }
+    if (props.type == "doubleColumn"){
+        return(
+            <div>
+                {/* insert jsx for the 2 column thumbnail here */}
+            </div>
+        )
+    }
+    if (props.type == "smallThumbnail"){
+        return(
+            <div>
+                {/* insert jsx for the small thumbnail here */}
+            </div>
+        )
+    }
 }
 
 export default Block;
--- src/components/Thumbnails Block/Thumbnails.block.stories.js
+++ src/components/Thumbnails Block/Thumbnails.block.stories.js
@@ -8,6 +8,29 @@ storiesOf('Thumbnail Block', module)
 // 1 column white
     .add('1 Column White', () => <Block 
         label="Apple Macbook Pro" 
-        type="columnWhite"
+        type="column"
         white
+    />)
+// 1 column grey
+    .add('1 Column Grey', () => <Block
+        label="Apple Macbook Pro"
+        type="column"
+        grey
+    />)
+
+// 1 column hover icons white (include in 1 column white?)
+
+// 1 column hover icons blue (include in 1 column grey?)
+
+// 2 column white (w/ grey hover)
+    .add('2 Column White', () => <Block
+        label="Apple Macbook Pro"
+        type="doubleColumn"
+        white
+    />)
+
+// small thumbnail
+    .add('Small Thumbnail', () => <Block
+        label="Beats Solo 2 On Ear Headphones"
+        type="smallThumbnail"
     />)
